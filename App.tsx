edddbc62ba39e555ba630d1dc78b1267951f788c
Fred <ricardofredt@hotmail.com>
2021-07-20 23:34:07 -0300

    implemented map props with coordinates, zoom and markers

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import {View, requireNativeComponent, StyleSheet} from 'react-native';
+import {View, requireNativeComponent, StyleSheet, Alert} from 'react-native';
 
 const TomTomMap = requireNativeComponent('MapViewManager');
 
 const App = () => {
   return (
     <>
-      <TomTomMap style={StyleSheet.absoluteFillObject} />
+      <TomTomMap
+        onMapReady={this.onMapReady}
+        markers={[
+          {lat: -29.572263, lng: -50.793078, label: 'Igrejinha'},
+          {lat: -29.64985, lng: -50.781169, label: 'Taquara'},
+        ]}
+        mapZoom={15}
+        mapCenter={{lat: -29.572263, lng: -50.793078}}
+        style={StyleSheet.absoluteFillObject}
+      />
     </>
   );
 };

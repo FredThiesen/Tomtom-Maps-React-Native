fd0491dbb80b892c3ae63be253e5f89766dc3012
Fred <ricardofredt@hotmail.com>
2021-07-20 21:52:36 -0300

    added TomTomMap class

--- android/app/src/main/java/com/myapp/NativeModules/MapViewManager.java
+++ android/app/src/main/java/com/myapp/NativeModules/MapViewManager.java
@@ -1,11 +1,15 @@
 package com.myapp.NativeModules;
 
+
+import android.content.Context;
+import androidx.annotation.NonNull;
 import com.tomtom.online.sdk.map.MapView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+
 
 import javax.annotation.Nonnull;
 
@@ -19,7 +23,8 @@ public class MapViewManager extends SimpleViewManager<View> {
     @Nonnull
     @Override
     protected View createViewInstance(@Nonnull ThemedReactContext reactContext) {
-        MapView mapView = new MapView(reactContext);
-        return  mapView;
+        TomTomMap tomTomMap = new TomTomMap(reactContext);
+        tomTomMap.onResume();
+        return tomTomMap;
     }
 }
---
+++ android/app/src/main/java/com/myapp/NativeModules/TomTomMap.java
@@ -0,0 +1,13 @@
+package com.myapp.NativeModules;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.tomtom.online.sdk.map.MapView;
+
+public class TomTomMap extends MapView {
+    public TomTomMap(@NonNull Context context) {
+        super(context);
+    }
+}

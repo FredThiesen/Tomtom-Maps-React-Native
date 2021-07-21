e916566016333421c6c054c7b80e27928a731d37
Fred <ricardofredt@hotmail.com>
2021-07-20 21:57:42 -0300

    added props to TomTomMaps

--- android/app/src/main/java/com/myapp/NativeModules/MapViewManager.java
+++ android/app/src/main/java/com/myapp/NativeModules/MapViewManager.java
@@ -9,9 +9,15 @@ import android.widget.TextView;
 
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.annotations.ReactProp;
 
+import java.util.Map;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 public class MapViewManager extends SimpleViewManager<View> {
     @Nonnull
@@ -26,5 +32,29 @@ public class MapViewManager extends SimpleViewManager<View> {
         TomTomMap tomTomMap = new TomTomMap(reactContext);
         tomTomMap.onResume();
         return tomTomMap;
+    }
+
+    @ReactProp(name = "mapCenter")
+    public void setCenter(TomTomMap mapView, @Nullable ReadableMap center) {
+        mapView.setMapCenter(center);
+    }
+
+    @ReactProp(name = "markers")
+    public void setMarkers(TomTomMap mapView, @Nullable ReadableArray markers) {
+        mapView.setMarkers(markers);
+    }
+
+    @ReactProp(name = "mapZoom")
+    public void setMapZoom(TomTomMap mapView, @Nullable Integer zoom) {
+        mapView.setMapZoom(zoom);
+    }
+
+
+    @Nullable
+    @Override
+    public Map getExportedCustomBubblingEventTypeConstants() {
+        return MapBuilder.builder()
+                .put("onMapReady", MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", "onMapReady")))
+                .build();
     }
 }
